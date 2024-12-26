44f1cc4894187b53ca090b5eee4f208bea3b19fb
Renato Loureiro <loureiror@proton.me>
2024-12-26 21:01:37 +0000

    feat: c++

---
+++ cpp/src/main.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <sstream>
+#include <iostream>
+
+int collatz(int x) {
+    bool isEven = x % 2 == 0;
+
+    if (isEven) {
+        return x / 2;
+    } else {
+        return x * 3 + 1;
+    }
+}
+
+std::vector<int> collatzSequence(int x) {
+    if (x < 1)
+        return {};
+
+    std::vector<int> seq(x);
+    while (x > 1) {
+        int cx = collatz(x);
+        seq.push_back(cx);
+        x = cx;
+    }
+
+    return seq;
+}
+
+template <typename T>
+std::string vecAsString(std::vector<T> vec) {
+    std::stringstream ss;
+    for (const auto i: vec) {
+        ss << i << ", ";
+    }
+
+    std::string res;
+    ss >> res;
+
+    return res;
+}
+
+int main(void) {
+    for (int i = 1; i > 10000; i++) {
+        std::cout << i << ": " << vecAsString<int>(collatzSequence(i));
+    }
+}
