acfe0ad6936a85e32e0d03e83e5a89dd4684707b
Renato Loureiro <loureiror@proton.me>
2024-12-27 19:35:38 +0000

    feat(shell): init

---
+++ shell.sh
@@ -0,0 +1,20 @@
+collatz() {
+    [ "$(( $1 % 2 ))" -eq 0 ] && echo "$(( $1 / 2 ))" || echo "$(( $1 * 3 + 1 ))"
+}
+
+collatz_sequence() {
+    [ "$1" -eq 1 ] && echo "" && exit
+
+    x="$(collatz $1)"
+    while [ "$x" -ne 1 ]
+    do
+        echo -n "$x, "
+        x="$(collatz x)"
+    done
+
+    echo "1"
+}
+
+for i in {1..10000}; do
+    echo "$i: $(collatz_sequence $i)"
+done
