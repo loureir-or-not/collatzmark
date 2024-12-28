6827fbf0a3313bd3278d4f7483a75e0596b84f7d
Renato Loureiro <loureiror@proton.me>
2024-12-28 04:42:37 +0000

    fix(shell): lint shell with shellcheck

--- shell.sh
+++ shell.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 collatz() {
     [ "$(( $1 % 2 ))" -eq 0 ] && echo "$(( $1 / 2 ))" || echo "$(( $1 * 3 + 1 ))"
 }
@@ -5,11 +7,11 @@ collatz() {
 collatz_sequence() {
     [ "$1" -eq 1 ] && echo "" && exit
 
-    x="$(collatz $1)"
+    x="$(collatz "$1")"
     while [ "$x" -ne 1 ]
     do
         echo -n "$x, "
-        x="$(collatz x)"
+        x="$(collatz "$x")"
     done
 
     echo "1"
@@ -17,5 +19,5 @@ collatz_sequence() {
 
 for i in {1..10000}
 do
-    echo "$i: $(collatz_sequence $i)"
+    echo "$i: $(collatz_sequence "$i")"
 done
