c1d3edf0e0c68565bf8b0c9bb3fc511c8dd1dafd
Renato Loureiro <loureiror@proton.me>
2024-12-28 15:18:00 +0000

    feat(elixir): init proper testing

--- elixir/lib/collatzmark.ex
+++ elixir/lib/collatzmark.ex
@@ -1,10 +1,20 @@
 defmodule Collatzmark do
+  @moduledoc """
+  This module does stuff with Collatz conjecture.
+  """
+
   def main(_args) do
     Enum.map(1..10000, fn x -> IO.puts("#{x}: #{collatz_sequence(x)}") end)
   end
 
   require Integer
 
+  @doc """
+  Returns the collatz integer of parameter `x`.
+
+  If x is odd, we multiply x by 3, then add one.
+  If x is even, we half it.
+  """
   def collatz(x) when Integer.is_even(x) do
     Integer.floor_div(x, 2)
   end
@@ -13,6 +23,12 @@ defmodule Collatzmark do
     3 * x + 1
   end
 
+  @doc """
+  Returns a sequence of collatz integers
+  using parameter `x` or 1 as a starting point.
+
+  Returns an empty array if given parameter 1.
+  """
   def collatz_sequence(1) do
     []
   end
--- elixir/test/collatzmark_test.exs
+++ elixir/test/collatzmark_test.exs
@@ -2,7 +2,11 @@ defmodule CollatzmarkTest do
   use ExUnit.Case
   doctest Collatzmark
 
-  test "greets the world" do
-    assert Collatzmark.hello() == :world
+  test "Collatz of {4, 5} are {2, 16}" do
+    assert({Collatzmark.collatz(4), Collatzmark.collatz(5)} == {2, 16})
+  end
+
+  test "The collatz sequence of {1, 2} is {[], [\"1,\"]}" do
+    assert({Collatzmark.collatz_sequence(1), Collatzmark.collatz_sequence(2)} == {[], ["1,"]})
   end
 end
