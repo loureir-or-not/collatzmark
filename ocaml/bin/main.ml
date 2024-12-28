c96dba8b2acae7059982ddc4678f3183f8c1b77b
Renato Loureiro <loureiror@proton.me>
2024-12-28 04:32:37 +0000

    fix(ocaml): format

--- ocaml/bin/main.ml
+++ ocaml/bin/main.ml
@@ -1,14 +1,10 @@
-let collatz x =
-    if x mod 2 == 0
-    then x / 2
-    else 3 * x + 1
+let collatz x = if x mod 2 == 0 then x / 2 else (3 * x) + 1
 
 let rec collatz_sequence x =
-    if x == 1
-    then ""
-    else string_of_int (collatz x) ^ " " ^ collatz_sequence (collatz x)
+  if x == 1 then ""
+  else string_of_int (collatz x) ^ " " ^ collatz_sequence (collatz x)
 
 let () =
-    for i = 1 to 10000 do
-        print_endline (string_of_int i ^ ": " ^ collatz_sequence i)
-    done
+  for i = 1 to 10000 do
+    print_endline (string_of_int i ^ ": " ^ collatz_sequence i)
+  done
